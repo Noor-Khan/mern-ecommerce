7267b29ea2907c475571d70011633fb07fe986f8
Noor Khan <nksoftdev@gmail.com>
2025-11-12 08:40:08 +0500

    add /api/product endpoint toget all products

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,16 @@ dotenv.config();
 const app = express();
 app.use(express.json()); // allow us to accept JSON data in the req.body
 
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error while fetching products", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+
 app.post("/api/products", async (req, res) => {
   const product = req.body;
   if (!product.name || !product.price || !product.image) {
@@ -35,14 +45,9 @@ app.delete("/api/products/:id", async (req, res) => {
       .status(200)
       .json({ success: true, message: `Product is deleted: ${id}` });
   } catch (error) {
-    console.error("Error while deleting product", error.message);
-    res.status(500).json({ success: false, message: error.message });
+    res.status(404).json({ success: false, message: "Product not found" });
   }
 });
-
-// app.get("/products", (req, res) => {
-//   res.send("There is no product right now!");
-// });
 
 app.listen(3000, () => {
   connectDB();
