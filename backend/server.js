feef5d0452d47282b9038e8420d7c523dce3cb49
Noor Khan <nksoftdev@gmail.com>
2025-11-12 07:54:41 +0500

    add first endpoint called /products to add a product with post method

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json()); // allow us to accept JSON data in the req.body
 
-app.get("/products", (req, res) => {
-  res.send("There is no product right now!");
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "please provide all the fields" });
+  }
+
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (err) {
+    console.error("Errro while creating product", err.message);
+    res.status(500).json({ success: false, message: "server errr" });
+  }
 });
+// app.get("/products", (req, res) => {
+//   res.send("There is no product right now!");
+// });
 
 app.listen(3000, () => {
   connectDB();
